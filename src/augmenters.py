7a1f8f8f2858ba4e0f5b106a3f01269a4f19a0db
Bartosz Stachowiak <sbartekt@op.pl>
2023-01-18 20:07:38 +0100

    chore: rename mask_image_augment to mask_image_augmenter

--- src/augmenters.py
+++ src/augmenters.py
@@ -8,7 +8,7 @@ def identical_augmenter(x: tf.Tensor) -> tf.Tensor:
     new = tf.identity(x)
     return new
 
-def mask_image_augment(images: tf.Tensor, mask_generator: Callable[[tf.Tensor], np.ndarray], max_val: float) -> tf.Tensor:
+def mask_image_augmenter(images: tf.Tensor, mask_generator: Callable[[tf.Tensor], np.ndarray], max_val: float) -> tf.Tensor:
     masks = mask_generator(images.shape[0])
     augmented = tf.where(masks == 0, images, max_val) 
     return augmented
