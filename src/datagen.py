b887ba10ef258aa6b1bdf9ab073a7304db2e129d
Bartosz Stachowiak <sbartekt@op.pl>
2023-01-24 16:02:40 +0100

    chore: add apropriate annotations

--- src/datagen.py
+++ src/datagen.py
@@ -1,28 +1,29 @@
 import tensorflow as tf
-import numpy as np
 
-from typing import Callable
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
 
 
 class DatasetFillGenerator(tf.keras.utils.Sequence):
     def __init__(
         self,
         dataset: tf.data.Dataset,
-        image_augmenter: Callable[[tf.Tensor], tuple[tf.Tensor, tf.Tensor]],
+        image_augmenter: Callable[[tf.Tensor], tuple[T, tf.Tensor]],
         shuffle: bool = True,
     ) -> None:
         self.dataset = dataset
         self.image_augmenter = image_augmenter
         self.shuffle = shuffle
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.dataset)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> tuple[T, tf.Tensor]:
         for batch in self.dataset.skip(idx).take(1):
             return self._process_batch(batch)
 
-    def _process_batch(self, batch):
+    def _process_batch(self, batch: tf.Tensor) -> tuple[T, tf.Tensor]:
         augmented, batch = self.image_augmenter(batch)
         return augmented, batch
 
