52c330bd31201c53a2ee4665d6591eef4f9a0591
Bartosz Stachowiak <sbartekt@op.pl>
2023-01-24 15:38:27 +0100

    update: rename n_layers param to n_blocks for consistency

--- src/builders/unet.py
+++ src/builders/unet.py
@@ -7,7 +7,7 @@ class UNETBuilder:
         input_shape,
         output_shape,
         n_filters=64,
-        n_layers=4,
+        n_blocks=4,
         n_convs=2,
         activation="relu",
         last_activation="sigmoid",
@@ -16,7 +16,7 @@ class UNETBuilder:
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.n_filters = n_filters
-        self.n_layers = n_layers
+        self.n_blocks = n_blocks
         self.n_convs = n_convs
         self.activation = activation
         self.last_activation = last_activation
@@ -26,7 +26,7 @@ class UNETBuilder:
         inputs = tf.keras.Input(shape=self.input_shape)
         x = inputs
         skips = []
-        for i in range(self.n_layers):
+        for i in range(self.n_blocks):
             for _ in range(self.n_convs):
                 x = tf.keras.layers.Conv2D(
                     self.n_filters * 2**i,
@@ -38,18 +38,18 @@ class UNETBuilder:
             x = tf.keras.layers.MaxPool2D(2)(x)
             if i > 0 and self.dropout_rate > 0:
                 x = tf.keras.layers.Dropout(self.dropout_rate)(x)
-        for i in range(self.n_layers):
+        for i in range(self.n_blocks):
             x = tf.keras.layers.Conv2D(
-                self.n_filters * 2 ** (self.n_layers - i - 1),
+                self.n_filters * 2 ** (self.n_blocks - i - 1),
                 3,
                 padding="same",
                 activation=self.activation,
             )(x)
             x = tf.keras.layers.UpSampling2D(2)(x)
-            x = tf.keras.layers.Concatenate()([x, skips[self.n_layers - i - 1]])
+            x = tf.keras.layers.Concatenate()([x, skips[self.n_blocks - i - 1]])
             for _ in range(self.n_convs):
                 x = tf.keras.layers.Conv2D(
-                    self.n_filters * 2 ** (self.n_layers - i - 1),
+                    self.n_filters * 2 ** (self.n_blocks - i - 1),
                     3,
                     padding="same",
                     activation=self.activation,
