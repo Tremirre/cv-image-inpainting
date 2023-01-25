fc5c62d11e32505b72deafaa33a4e84cdd6337b7
Bartosz Stachowiak <sbartekt@op.pl>
2023-01-25 19:54:39 +0100

    feat: add new controls to client app

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,11 @@
 const imageInput = document.getElementById('image-input');
 const predImage = document.getElementById('pred-image');
 const inpaintButton = document.getElementById('inpaint-button');
+const clearMaskButton = document.getElementById('clear-mask-button');
+const refreshModelsButton = document.getElementById('refresh-models-button');
+const brushSizeInput = document.getElementById('brush-size-input');
+const modelSelect = document.getElementById('model-select');
+const srcImage = document.getElementById('src-image');
 const srcCanvas = document.getElementById('src-canvas');
 const canvasCtx = srcCanvas.getContext('2d');
 const backendURL = 'http://127.0.0.1:5000';
@@ -10,17 +15,36 @@ srcCanvas.height = 256;
 srcCanvas.width = 256;
 canvasCtx.lineWidth = 12;
 canvasCtx.lineCap = 'round';
-canvasCtx.strokeStyle = 'rgb(255, 255, 255)';
+canvasCtx.strokeStyle = 'rgb(255, 0, 0)';
 
-const image = new Image();
+clearMaskButton.addEventListener('click', (event) => {
+    canvasCtx.clearRect(0, 0, 256, 256);
+})
+
+refreshModelsButton.addEventListener('click', (event) => {
+    fetch(`${backendURL}/models`).then((response) => {
+        return response.json();
+    }).then((data) => {
+        modelSelect.innerHTML = '';
+        for (let model of data) {
+            const option = document.createElement('option');
+            option.value = model;
+            option.text = model;
+            modelSelect.appendChild(option);
+        }
+    });
+})
+refreshModelsButton.click();
+
+brushSizeInput.addEventListener('change', (event) => {
+    canvasCtx.lineWidth = event.target.value;
+})
 
 imageInput.addEventListener('change', (event) => {
     const reader = new FileReader();
     reader.onload = (event) => {
-        image.src = event.target.result;
-        image.onload = () => {
-            canvasCtx.drawImage(image, 0, 0, 256, 256);
-        }
+        srcImage.src = event.target.result;
+        canvasCtx.clearRect(0, 0, 256, 256);
     }
     reader.readAsDataURL(event.target.files[0]);
 });
@@ -42,17 +66,31 @@ srcCanvas.addEventListener('mouseup', (event) => {
     drawing = false;
 })
 
+srcCanvas.addEventListener('mouseleave', (event) => {
+    drawing = false;
+})
+
 
 inpaintButton.addEventListener('click', (event) => {
-    const dataURL = srcCanvas.toDataURL('image/jpeg', 1.0);
-    const base64 = dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
-    const pureBase64 = base64.split(',')[1];
+    // serialize canvas to base64
+    const dataURL = srcCanvas.toDataURL('image/png');
+    const maskBase64 = dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+
+    // serialize srcImage to base64
+    const srcDataURL = srcImage.src;
+    const srcBase64 = srcDataURL.replace(/^data:image\/(png|jpeg);base64,/, '');
+
+    const selectedModel = modelSelect.options[modelSelect.selectedIndex].value;
     fetch(`${backendURL}/predict`, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'image/jpeg+base64'
+        headers: {  
+            'Content-Type': 'application/json',
         },
-        body: pureBase64
+        body: JSON.stringify({
+            image: srcBase64,
+            mask: maskBase64,
+            model: selectedModel,
+        })
     }).then((response) => {
         return response.blob();
     }).then((data) => {
