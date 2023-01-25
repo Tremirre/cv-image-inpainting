b6b7c64203592329ca0b65e1247a42735c081bb5
Bartosz Stachowiak <sbartekt@op.pl>
2023-01-25 19:36:12 +0100

    feat: add multi-model support for backend app

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 import json
 import tensorflow as tf
 import logging
@@ -12,7 +13,9 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-MODEL = tf.saved_model.load("models/unet_dummy")
+ALL_MODELS = {}
+for model in os.listdir("models"):
+    ALL_MODELS[model] = tf.saved_model.load(os.path.join("models", model))
 
 
 @app.route("/")
@@ -22,40 +25,59 @@ def hello_world():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    binary_jpeg = request.data
-    if not binary_jpeg:
+    data = request.get_json()
+    binary_jpg = data.get("image")
+    mask_png = data.get("mask")
+    model_name = data.get("model", "unet_working")
+    used_model = ALL_MODELS.get(model_name)
+    errors = {}
+    if not used_model:
+        errors["model"] = "Invalid model provided"
+    if not binary_jpg:
+        errors["image"] = "No image provided"
+    if not mask_png:
+        errors["mask"] = "No mask provided"
+    if errors:
         return Response(
-            json.dumps({"error": "No image provided"}),
+            json.dumps(errors),
             status=400,
             mimetype="application/json",
         )
     try:
-        if request.headers["Content-Type"] == "image/jpeg+base64":
-            binary_jpeg = base64.b64decode(binary_jpeg)
-        decoded_image = tf.io.decode_jpeg(binary_jpeg)
+        binary_jpg = base64.b64decode(binary_jpg)
+        mask_png = base64.b64decode(mask_png)
+        decoded_image = tf.io.decode_png(binary_jpg, channels=3)
+        decoded_mask = tf.io.decode_png(mask_png, channels=3)
     except tf.errors.InvalidArgumentError:
         return Response(
-            json.dumps({"error": "Invalid image provided - should be a jpg image"}),
+            json.dumps({"error": "Invalid image provided - should be a png image"}),
             status=400,
             mimetype="application/json",
         )
     logging.info(f"Decoded image shape: {decoded_image.shape}")
+    logging.info(f"Decoded mask shape: {decoded_mask.shape}")
     resized_image = tf.image.resize(decoded_image, (256, 256)) / 255
     overlay_mask = tf.where(
-        tf.reduce_sum(resized_image, axis=-1, keepdims=True) == 3, 1.0, 0.0
+        tf.reduce_sum(decoded_mask, axis=-1, keepdims=True) > 0, 1.0, 0.0
     )
+    resized_image = tf.where(overlay_mask == 1.0, 0, resized_image)
     mask_size = tf.reduce_sum(overlay_mask)
     logging.info(f"Masked pixels: {mask_size} ({mask_size / 256 ** 2 * 100:.2f} %)")
 
     resized_image = tf.concat([resized_image, overlay_mask], axis=-1)
     reshaped_image = tf.reshape(resized_image, (1, 256, 256, 4))
     logging.info(f"Reshaped image shape: {reshaped_image.shape}")
-    filled_image = MODEL(reshaped_image)
+    filled_image = used_model(reshaped_image)
     filled_image = tf.reshape(filled_image, (256, 256, 4))
     filled_image = filled_image[:, :, :3] * 255
     filled_image = tf.cast(filled_image, tf.uint8)
     filled_image = tf.image.encode_jpeg(filled_image)
     return Response(filled_image.numpy(), mimetype="image/jpeg", status=200)
+
+
+@app.route("/models")
+def models():
+    return Response(json.dumps(list(ALL_MODELS.keys())), mimetype="application/json")
 
 
 if __name__ == "__main__":
